94606cf1e213c6cfc7841dded13c7ba459b58371
Nils Destoop <nils.destoop@iodigital.com>
2025-02-26 16:24:07 +0100

    Fix coding standards for enum

--- src/Enum/Availability.php
+++ src/Enum/Availability.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace CultuurNet\SearchV3\Enum;
 
-enum Availability {
-  case Available;
-  case Unavailable;
+enum Availability
+{
+    case Available;
+    case Unavailable;
 }
