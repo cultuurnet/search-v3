2a3586c828c1424cf39208ee228bc6ae9c11e672
Wouter VanIslander <wouter.vanelslander@iodigital.com>
2025-04-25 09:04:39 +0200

    PUBLIQ-1275: Added priceInfo groupPrice property

--- src/ValueObjects/PriceInfo.php
+++ src/ValueObjects/PriceInfo.php
@@ -5,70 +5,88 @@ declare(strict_types=1);
 namespace CultuurNet\SearchV3\ValueObjects;
 
 use JMS\Serializer\Annotation\Type;
+use JMS\Serializer\Annotation\SerializedName;
 
 final class PriceInfo
 {
-    /**
-     * @var string|null
-     * @Type("string")
-     */
-    private $category;
+  /**
+   * @var string|null
+   * @Type("string")
+   */
+  private $category;
 
-    /**
-     * @var TranslatedString|null
-     * @Type("CultuurNet\SearchV3\ValueObjects\TranslatedString")
-     */
-    private $name;
+  /**
+   * @var TranslatedString|null
+   * @Type("CultuurNet\SearchV3\ValueObjects\TranslatedString")
+   */
+  private $name;
 
-    /**
-     * @var string|null
-     * @Type("string")
-     */
-    private $priceCurrency;
+  /**
+   * @var string|null
+   * @Type("string")
+   */
+  private $priceCurrency;
 
-    /**
-     * @var float|null
-     * @Type("float")
-     */
-    private $price;
+  /**
+   * @var float|null
+   * @Type("float")
+   */
+  private $price;
 
-    public function getCategory(): ?string
-    {
-        return $this->category;
-    }
+  /**
+   * @var bool|null
+   * @Type("bool")
+   * @SerializedName("groupPrice")
+   */
+  private $isGroupPrice;
 
-    public function setCategory(string $category): void
-    {
-        $this->category = $category;
-    }
+  public function getCategory(): ?string
+  {
+    return $this->category;
+  }
 
-    public function getName(): ?TranslatedString
-    {
-        return $this->name;
-    }
+  public function setCategory(string $category): void
+  {
+    $this->category = $category;
+  }
 
-    public function setName(TranslatedString $name): void
-    {
-        $this->name = $name;
-    }
+  public function getName(): ?TranslatedString
+  {
+    return $this->name;
+  }
 
-    public function getPriceCurrency(): ?string
-    {
-        return $this->priceCurrency;
-    }
+  public function setName(TranslatedString $name): void
+  {
+    $this->name = $name;
+  }
 
-    public function setPriceCurrency(string $priceCurrency): void
-    {
-        $this->priceCurrency = $priceCurrency;
-    }
+  public function getPriceCurrency(): ?string
+  {
+    return $this->priceCurrency;
+  }
 
-    public function getPrice(): ?float
-    {
-        return $this->price;
-    }
+  public function setPriceCurrency(string $priceCurrency): void
+  {
+    $this->priceCurrency = $priceCurrency;
+  }
 
-    public function setPrice(float $price): void
-    {
-        $this->price = $price;
-    }
+  public function getPrice(): ?float
+  {
+    return $this->price;
+  }
+
+  public function setPrice(float $price): void
+  {
+    $this->price = $price;
+  }
+
+  public function isGroupPrice(): bool
+  {
+    return $this->isGroupPrice ?? false;
+  }
+
+  public function setIsGroupPrice(?bool $isGroupPrice): void
+  {
+    $this->isGroupPrice = $isGroupPrice;
+  }
 }
